680fca21332b6a7003e73b9e4bcb58e3d792328a
Frank Waldheim <fw@123k.org>
2019-02-14 22:01:22 +0100

    we send SSE now

--- src/main/kotlin/de/wipu/mosty/MapController.kt
+++ src/main/kotlin/de/wipu/mosty/MapController.kt
@@ -2,6 +2,7 @@ package de.wipu.mosty
 
 import org.bson.types.ObjectId
 import org.springframework.http.MediaType
+import org.springframework.http.codec.ServerSentEvent
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import java.time.Duration
@@ -12,16 +13,27 @@ import java.util.*
 @RequestMapping(path = ["/map"])
 class Controller(val trackPointRepository: TrackPointRepository, val trackRepository: TrackRepository) {
 
-    @GetMapping(path = ["/tracks"], produces = [MediaType.APPLICATION_STREAM_JSON_VALUE])
+    @CrossOrigin
+    @GetMapping(path = ["/tracks"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     @ResponseBody
-    fun getLatestTrack(): Flux<Track> {
+    fun getLatestTrack(): Flux<ServerSentEvent<Track>> {
         return trackRepository.findByStartAfter(Date.from(Instant.parse("2018-05-01T00:00:00.00Z")))
+                .map { track: Track ->
+                    ServerSentEvent
+                            .builder<Track>()
+                            .data(track)
+                            .event("track")
+                            .build()
+                }
+                .delayElements(Duration.ofSeconds(1))
+
     }
 
+    @CrossOrigin
     @GetMapping(value = ["/track/{id}/points"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     @ResponseBody
     fun trackPoints(@PathVariable("id") trackId: String): Flux<TrackPoint> {
-        return  trackPointRepository.findByTrackId(ObjectId(trackId))
+        return trackPointRepository.findByTrackId(ObjectId(trackId))
     }
 
 }
